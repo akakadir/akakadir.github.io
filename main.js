2a4e803238d5ec61bab4b6693b040c8775ba8955
kadir <101037506+akakadir@users.noreply.github.com>
2023-11-17 20:21:15 +0300

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-$.ajax({
-  url: "https://spotify.aidenwallis.co.uk/u/63a218d8d1def76279e0eec8",
-  method: "GET"
-}).then(function(data) {
-  adminlisten = data;
-  $("#track").text(adminlisten);
-});
