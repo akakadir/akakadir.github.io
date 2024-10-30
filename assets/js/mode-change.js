639c52570411b906f6dcece995f5ca62413ed452
Kadir <101037506+akakadir@users.noreply.github.com>
2024-10-31 00:20:33 +0300

    Update mode-change.js

--- assets/js/mode-change.js
+++ assets/js/mode-change.js
@@ -2,6 +2,11 @@
 function changeMode() {
     // detect body data-theme attribute
     let theme = document.body.getAttribute("data-theme");
+    // on first load, check if user has a preference in cookie
+    const isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    if (theme === null) {
+        theme = isDark ? "dark" : "light";
+    }
     // toggle theme
     theme = theme === "dark" ? "light" : "dark";
     document.body.setAttribute("data-theme", theme);
@@ -9,18 +14,23 @@ function changeMode() {
     document.cookie = `theme=${theme}; max-age=31536000; SameSite=Lax; path=/`;
 }
 
-// auto change theme based on user's preference and cookie
-function autoChangeMode() {
-    let theme = document.body.getAttribute("data-theme");
+// add event load to check cookie for theme preference and set the theme
+window.addEventListener("load", () => {
     // check if user has a preference in cookie
-    const cookieTheme = getCookie("theme");
-    if (cookieTheme) {
-        theme = cookieTheme;
-    } else {
-        const isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    const theme = getCookie("theme");
+    if (theme) {
+        document.body.setAttribute("data-theme", theme);
+    }
+});
+// auto change theme based data-theme attribute
+function autoChangeMode() {
+    // detect body data-theme attribute
+    let theme = document.body.getAttribute("data-theme");
+    // on first load, check if user has a preference in cookie
+    const isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    if (theme === null) {
         theme = isDark ? "dark" : "light";
     }
-    document.body.setAttribute("data-theme", theme);
 }
 
 // get cookie by name
@@ -31,15 +41,10 @@ function getCookie(name) {
         return parts.pop().split(";").shift();
     }
 }
-
 // make it priority to load before other scripts
 document.addEventListener("DOMContentLoaded", function () {
-    // set initial theme based on cookie or preference
-    autoChangeMode();
-
     // add event listener to mode button
     document.getElementById("mode").addEventListener("click", changeMode);
-
     // add event listener to auto change mode
     window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", autoChangeMode);
 });
