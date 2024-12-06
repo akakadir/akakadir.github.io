e5168cb75b725650cfad7a3010022d2b7fb902cb
Kadir <101037506+akakadir@users.noreply.github.com>
2024-12-06 17:56:30 +0300

    Update mode-change.js

--- assets/js/mode-change.js
+++ assets/js/mode-change.js
@@ -1,18 +1,16 @@
-function updateGiscusTheme(theme) {
-    const giscusTheme = theme === 'dark' ? 'https://akakadir.github.io/assets/css/giscus_dark.css' : 'https://akakadir.github.io/assets/css/giscus_light.css';
-    const iframe = document.querySelector('iframe.giscus-frame');
-    const giscusScript = document.getElementById('giscus');
-    
-    if (giscusScript) giscusScript.setAttribute('data-theme', giscusTheme);
-    if (iframe) iframe.contentWindow.postMessage({ giscus: { setConfig: { theme: giscusTheme } } }, 'https://giscus.app');
-}
-
 function changeMode() {
     const theme = document.body.getAttribute("data-theme") === 'dark' ? 'light' : 'dark';
     document.body.setAttribute("data-theme", theme);
     localStorage.setItem('theme', theme);
     document.cookie = `theme=${theme}; max-age=31536000; SameSite=Lax; path=/`;
+
     updateGiscusTheme(theme);
+}
+
+function updateGiscusTheme(theme) {
+    const giscusTheme = theme === 'dark' ? 'https://akakadir.github.io/assets/css/giscus_dark.css' : 'https://akakadir.github.io/assets/css/giscus_light.css';
+    const giscusScript = document.getElementById('giscus');
+    if (giscusScript) giscusScript.setAttribute('data-theme', giscusTheme);
 }
 
 function loadTheme() {
