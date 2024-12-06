b6585f5f93afc5641a2c8aaecc49fd3f86d133e1
Kadir <101037506+akakadir@users.noreply.github.com>
2024-12-06 17:35:35 +0300

    Update mode-change.js

--- assets/js/mode-change.js
+++ assets/js/mode-change.js
@@ -4,33 +4,24 @@ function changeMode() {
     localStorage.setItem('theme', theme);
     document.cookie = `theme=${theme}; max-age=31536000; SameSite=Lax; path=/`;
 
-    changeGiscusTheme(theme);
+    updateGiscusTheme(theme);
 }
 
-function changeGiscusTheme(theme) {
+function updateGiscusTheme(theme) {
     const giscusTheme = theme === 'dark' ? 'https://akakadir.github.io/assets/css/giscus_dark.css' : 'https://akakadir.github.io/assets/css/giscus_light.css';
-    const iframe = document.querySelector('iframe.giscus-frame');
-    if (iframe) {
-        iframe.contentWindow.postMessage({ giscus: { setConfig: { theme: giscusTheme } } }, 'https://giscus.app');
-        localStorage.setItem('giscusTheme', giscusTheme);
-        document.cookie = `giscusTheme=${giscusTheme}; max-age=31536000; SameSite=Lax; path=/`;
-    }
+    const giscusScript = document.getElementById('giscus');
+    if (giscusScript) giscusScript.setAttribute('data-theme', giscusTheme);
 }
 
-function checkThemeOnLoad() {
+function loadTheme() {
     const theme = localStorage.getItem("theme") || (window.matchMedia("(prefers-color-scheme: dark)").matches ? 'dark' : 'light');
     document.body.setAttribute("data-theme", theme);
-    const giscusTheme = localStorage.getItem("giscusTheme") || (theme === 'dark' ? 'https://akakadir.github.io/assets/css/giscus_dark.css' : 'https://akakadir.github.io/assets/css/giscus_light.css');
-    
-    const iframe = document.querySelector('iframe.giscus-frame');
-    if (iframe) {
-        iframe.contentWindow.postMessage({ giscus: { setConfig: { theme: giscusTheme } } }, 'https://giscus.app');
-    }
+    updateGiscusTheme(theme);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("mode")?.addEventListener("click", () => changeMode());
+    document.getElementById("mode")?.addEventListener("click", changeMode);
     window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", changeMode);
 });
 
-window.addEventListener("load", checkThemeOnLoad);
+window.addEventListener("load", loadTheme);
