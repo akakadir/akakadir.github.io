6236f42017c8e75b0f330b58ee1312b4575903c3
Kadir <101037506+akakadir@users.noreply.github.com>
2024-12-06 21:21:27 +0300

    Update mode-change.js

--- assets/js/mode-change.js
+++ assets/js/mode-change.js
@@ -3,13 +3,14 @@ function changeMode() {
     document.body.setAttribute("data-theme", theme);
     localStorage.setItem('theme', theme);
     document.cookie = `theme=${theme}; max-age=31536000; SameSite=Lax; path=/`;
+
     changeGiscusTheme(theme);
 }
 
 function changeGiscusTheme(theme) {
     const giscusTheme = theme === 'dark' ? 'https://akakadir.github.io/assets/css/giscus_dark.css' : 'https://akakadir.github.io/assets/css/giscus_light.css';
     const iframe = document.querySelector('iframe.giscus-frame');
-    if (iframe && iframe.contentWindow) {
+    if (iframe) {
         iframe.contentWindow.postMessage({ giscus: { setConfig: { theme: giscusTheme } } }, 'https://giscus.app');
         localStorage.setItem('giscusTheme', giscusTheme);
         document.cookie = `giscusTheme=${giscusTheme}; max-age=31536000; SameSite=Lax; path=/`;
@@ -19,19 +20,19 @@ function changeGiscusTheme(theme) {
 function checkThemeOnLoad() {
     const theme = localStorage.getItem("theme") || (window.matchMedia("(prefers-color-scheme: dark)").matches ? 'dark' : 'light');
     document.body.setAttribute("data-theme", theme);
+
+    // Giscus temasını her zaman doğru ayarla
     const giscusTheme = localStorage.getItem("giscusTheme") || (theme === 'dark' ? 'https://akakadir.github.io/assets/css/giscus_dark.css' : 'https://akakadir.github.io/assets/css/giscus_light.css');
+    
     const iframe = document.querySelector('iframe.giscus-frame');
-    if (iframe && iframe.contentWindow) {
+    if (iframe) {
         iframe.contentWindow.postMessage({ giscus: { setConfig: { theme: giscusTheme } } }, 'https://giscus.app');
     }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("mode")?.addEventListener("click", changeMode);
-    window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", () => {
-        const currentTheme = document.body.getAttribute("data-theme");
-        changeMode(); // Tema değiştiğinde otomatik olarak güncelle
-    });
+    document.getElementById("mode")?.addEventListener("click", () => changeMode());
+    window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", changeMode);
 });
 
 window.addEventListener("load", checkThemeOnLoad);
