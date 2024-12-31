b9bfed019cea799f403bc496b86fd0929e091b18
Kadir <101037506+akakadir@users.noreply.github.com>
2024-12-31 06:09:06 +0300

    Update spotify.js

--- assets/js/spotify.js
+++ assets/js/spotify.js
@@ -2,6 +2,6 @@ setInterval(function(){
 fetch('https://spotify.aidenwallis.co.uk/u/63a218d8d1def76279e0eec8')
 .then(res => res.text())
 .then((textResponse) => {
-document.querySelector('#sp-wg').textContent = textResponse.replace('User is currently not playing any music on Spotify.', 'kendimi').replace('Error: Request failed with status code 429', 'kendimi');
+document.querySelector('#sp-wg').textContent = textResponse.replace('User is currently not playing any music on Spotify.', 'kafamı').replace('Error: Request failed with status code 429', 'kendimi');
  });
 }, 3000);
