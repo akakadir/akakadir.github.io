14ef91c37d78d336a736d3114e7eff2620f76058
Kadir <101037506+akakadir@users.noreply.github.com>
2025-01-15 15:10:59 +0300

    Update translate.js

--- assets/js/translate.js
+++ assets/js/translate.js
@@ -1,36 +1,40 @@
-    let isLoading = false, debounceTimer;
-    function googleTranslateElementInit() {
-        new google.translate.TranslateElement({ includedLanguages: 'tr,en,de,fr,es' }, 'translate');
+let isLoading = false, debounceTimer;
+
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement({ includedLanguages: 'tr,en,de,fr,es' }, 'translate');
+    isLoading = false;
+}
+
+function resetGoogleTranslate() {
+    if (isLoading) return;
+    isLoading = true;
+
+    const translateElement = document.getElementById('translate');
+    translateElement.innerHTML = "";
+
+    const existingScript = document.getElementById('google-translate-script');
+    if (existingScript) {
+        existingScript.remove();
+    }
+    const script = document.createElement('script');
+    script.id = 'google-translate-script';
+    script.src = `https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit&ts=${Date.now()}`;
+    script.onload = () => {
+        script.defer = true;
         isLoading = false;
-    }
-    function resetGoogleTranslate() {
-        if (isLoading) return;
-        isLoading = true;
+    };
 
-        const translateElement = document.getElementById('translate');
-        translateElement.innerHTML = "";
+    script.onerror = () => {
+        isLoading = false;
+        console.error("Google Çeviri hatası.");
+    };
 
-        const existingScript = document.getElementById('google-translate-script');
-        if (existingScript) {
-            existingScript.remove();
-        }
-        const script = document.createElement('script');
-        script.id = 'google-translate-script';
-        script.src = `https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit&ts=${Date.now()}`;
-        script.onload = () => {
-            script.defer = true;
-            isLoading = false;
-        };
+    document.head.appendChild(script);
+}
 
-        script.onerror = () => {
-            isLoading = false;
-            console.error("Google Çeviri hatası.");
-        };
+function resetGoogleTranslateDebounced() {
+    clearTimeout(debounceTimer);
+    debounceTimer = setTimeout(resetGoogleTranslate, 200);
+}
 
-        document.head.appendChild(script);
-    }
-    function resetGoogleTranslateDebounced() {
-        clearTimeout(debounceTimer);
-        debounceTimer = setTimeout(resetGoogleTranslate, 100);
-    }
-    ['DOMContentLoaded', 'pageshow', 'popstate'].forEach(event => window.addEventListener(event, resetGoogleTranslateDebounced));
+['DOMContentLoaded', 'pageshow', 'popstate'].forEach(event => window.addEventListener(event, resetGoogleTranslateDebounced));
