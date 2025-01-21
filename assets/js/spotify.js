8b574c32fd3b5c6551114fe822809ea1e5db148f
Kadir <101037506+akakadir@users.noreply.github.com>
2025-01-21 04:09:38 +0000

    Update spotify.js

--- assets/js/spotify.js
+++ assets/js/spotify.js
@@ -1,6 +1,8 @@
 function fetchLastTrack() {
     $.getJSON('https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=akakadir&api_key=99fb051e59a2e902e5a6f0981a4c6203&format=json', function(data) {
         var container = document.getElementById('lastfm');
+        var trackLink = document.getElementById('trackLink');
+        var noTrackMessage = document.getElementById('noTrackMessage');
 
         if (data.recenttracks && data.recenttracks.track && data.recenttracks.track.length > 0) {
             var track = data.recenttracks.track[0];
@@ -10,22 +12,20 @@ function fetchLastTrack() {
             var nowPlaying = track['@attr'] && track['@attr'].nowplaying;
 
             if (container) {
-                container.innerHTML = '';
                 if (nowPlaying) {
-                    var a = document.createElement('a');
-                    a.textContent = `${artist} - ${title}`;
-                    a.href = url;
-                    a.target = '_blank';
-                    container.appendChild(a);
+                    trackLink.textContent = `${artist} - ${title}`;
+                    trackLink.href = url;
+                    trackLink.target = '_blank';
+                    trackLink.style.display = 'inline-block';
+                    noTrackMessage.style.display = 'none';
                 } else {
-                    var span = document.createElement('span');
-                    span.textContent = "kafamı";
-                    container.appendChild(span);
+                    trackLink.style.display = 'none';
+                    noTrackMessage.style.display = 'inline-block';
                 }
             }
-        } else {
         }
     });
 }
+
 setInterval(fetchLastTrack, 3000);
 fetchLastTrack();
