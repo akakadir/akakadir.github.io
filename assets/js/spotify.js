519c442f4cb888ef521195cfe3b7b57577f7279f
Kadir <101037506+akakadir@users.noreply.github.com>
2025-01-21 06:38:39 +0300

    Update spotify.js

--- assets/js/spotify.js
+++ assets/js/spotify.js
@@ -1,7 +1,31 @@
-setInterval(function(){
-fetch('https://spotify.aidenwallis.co.uk/u/63a218d8d1def76279e0eec8')
-.then(res => res.text())
-.then((textResponse) => {
-document.querySelector('#sp-wg').textContent = textResponse.replace('User is currently not playing any music on Spotify.', 'kafamı').replace('Error: Request failed with status code 429', 'kendimi');
- });
-}, 3000);
+function fetchLastTrack() {
+    $.getJSON('https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=akakadir&api_key=99fb051e59a2e902e5a6f0981a4c6203&format=json', function(data) {
+        var container = document.getElementById('lastfm');
+
+        if (data.recenttracks && data.recenttracks.track && data.recenttracks.track.length > 0) {
+            var track = data.recenttracks.track[0];
+            var artist = track.artist["#text"];
+            var title = track.name;
+            var url = track.url;
+            var nowPlaying = track['@attr'] && track['@attr'].nowplaying;
+          
+            if (container) {
+                container.innerHTML = '';
+             
+                if (nowPlaying) {
+                    var a = document.createElement('a');
+                    a.textContent = `${artist} - ${title}`;
+                    a.href = url;
+                    a.target = '_blank';
+                    container.appendChild(a);
+                } else {
+                    container.innerHTML = "kafamı";
+                }
+            }
+        } else {
+            console.error("Son dinlenen parça bilgisi alınamadı.");
+        }
+    });
+}
+setInterval(fetchLastTrack, 3000);
+fetchLastTrack();
