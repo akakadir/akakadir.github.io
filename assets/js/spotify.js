f908fba8f2a05c3228e7a3ccab1e2301a4155551
Kadir <101037506+akakadir@users.noreply.github.com>
2025-03-20 19:36:16 +0300

    Delete assets/js/spotify.js

--- assets/js/spotify.js
+++
@@ -1,7 +0,0 @@
-setInterval(function(){
- fetch('https://spotify.aidenwallis.co.uk/u/63a218d8d1def76279e0eec8')
- .then(res => res.text())
- .then((textResponse) => {
- document.querySelector('#sp-wg').textContent = textResponse.replace('User is currently not playing any music on Spotify.', 'kafamı').replace('Error: Request failed with status code 429', 'oluşan bir hatayı');
-  });
- }, 3000);
