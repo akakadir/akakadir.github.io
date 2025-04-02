30264dbc22a7dcd321d2a2358878083988e5d0ed
Kadir <101037506+akakadir@users.noreply.github.com>
2025-04-03 00:19:57 +0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,49 @@ $(document).ready(function() {
   var fontSizeSlider = $("#fontSize");
   var fontSizeValue = $("#fontSizeValue");
   var fontSizeDisplay = $("#fontSizeDisplay");
-
+  var linkColorPicker = $("#linkColor");
+  var linkColorDisplay = $(".link-color");
+  var selectedColorText = $("#selectedColor");
+  
+  var originalFontSize;
+  var originalLinkColor;
+  
+  function saveSettings() {
+    var currentFontSize = fontSizeSlider.val();
+    var currentLinkColor = linkColorPicker.val();
+    
+    localStorage.setItem("fontSize", currentFontSize);
+    localStorage.setItem("linkColor", currentLinkColor);
+    
+    applySettings(currentFontSize, currentLinkColor);
+  }
+  
+  function applySettings(fontSize, linkColor) {
+    fontSizeValue.text(fontSize + "px");
+    fontSizeDisplay.css("font-size", fontSize + "px");
+    $("body").not("#ayarlar").css("font-size", fontSize + "px");
+    $("#ayarlar").css("font-size", "initial");
+    
+    linkColorDisplay.css("color", linkColor);
+    selectedColorText.text(linkColor);
+    $("a").not(".btn").css("color", linkColor);
+    
+    fontSizeSlider.val(fontSize);
+    linkColorPicker.val(linkColor);
+  }
+  
+  function loadSettings() {
+    var savedFontSize = localStorage.getItem("fontSize") || "16";
+    var savedLinkColor = localStorage.getItem("linkColor") || "#53a245";
+    
+    applySettings(savedFontSize, savedLinkColor);
+    
+    originalFontSize = savedFontSize;
+    originalLinkColor = savedLinkColor;
+  }
+  
+  loadSettings();
+  
   fontSizeSlider.on("input", function() {
     var size = fontSizeSlider.val();
     fontSizeValue.text(size + "px");
@@ -10,15 +52,24 @@ $(document).ready(function() {
     $("body").not("#ayarlar").css("font-size", size + "px");
     $("#ayarlar").css("font-size", "initial");
   });
-
-  var linkColorPicker = $("#linkColor");
-  var linkColorDisplay = $(".link-color");
-  var selectedColorText = $("#selectedColor");
-
+  
   linkColorPicker.on("input", function() {
     var selectedColor = linkColorPicker.val();
     linkColorDisplay.css("color", selectedColor);
     selectedColorText.text(selectedColor);
     $("a").not(".btn").css("color", selectedColor);
+  });
+  
+  $("#ayarlar").on("show", function() {
+    originalFontSize = fontSizeSlider.val();
+    originalLinkColor = linkColorPicker.val();
+  });
+  
+  $(".modal-footer .btn-primary").click(function() {
+    saveSettings();
+  });
+  
+  $(".modal-footer .btn-danger").click(function() {
+    applySettings(originalFontSize, originalLinkColor);
   });
 });
