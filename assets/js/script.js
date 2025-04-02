1a2024943e493e261705e5584e5e050d318d5951
Kadir <101037506+akakadir@users.noreply.github.com>
2025-04-02 19:43:00 +0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  var fontSizeSlider = $("#fontSize");
+  var fontSizeSlider = $("#fontSizeSlider");
   var fontSizeValue = $("#fontSizeValue");
   var fontSizeDisplay = $("#fontSizeDisplay");
 
