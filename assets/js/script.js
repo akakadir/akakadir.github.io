0106442c112455dfb68075209b8827e52c9037da
Kadir <101037506+akakadir@users.noreply.github.com>
2025-04-02 18:59:23 +0300

    Create script.js

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+  $(document).ready(function() {
+    var fontSizeSlider = $("#fontSize");
+    var fontSizeValue = $("#fontSizeValue");
+    var fontSizeDisplay = $("#fontSizeDisplay");
+    
+    fontSizeSlider.on("input", function() {
+      var size = fontSizeSlider.val();
+      fontSizeValue.text(size + "px");
+      fontSizeDisplay.css("font-size", size + "px");
+    });
+    
+  var linkColorPicker = $("#linkColor");
+  var linkColorDisplay = $(".link-color");
+  var selectedColorText = $("#selectedColor");
+
+  linkColorPicker.on("input", function() {
+    var selectedColor = linkColorPicker.val();
+    linkColorDisplay.css("color", selectedColor);
+    selectedColorText.text(selectedColor);
+  });
+  });
