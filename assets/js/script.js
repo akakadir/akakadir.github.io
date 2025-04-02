8e4918ac4bf3e1d9f862cdb47299d67026299868
Kadir <101037506+akakadir@users.noreply.github.com>
2025-04-02 22:19:20 +0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,4 +21,22 @@ $(document).ready(function() {
     selectedColorText.text(selectedColor);
     $("a").not(".btn").css("color", selectedColor);
   });
+
+  var isDarkMode = false;
+
+  function toggleTheme() {
+    const button = $("#mode");
+    
+    if (isDarkMode) {
+      button.addClass("btn-inverse");
+      isDarkMode = false;
+    } else {
+      button.removeClass("btn-inverse");
+      isDarkMode = true;
+    }
+  }
+
+  $("#mode").on("click", function() {
+    toggleTheme();
+  });
 });
