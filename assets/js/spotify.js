0981406aac816776b74a42b70f413b01a1ae4a75
Kadir <101037506+akakadir@users.noreply.github.com>
2025-04-11 17:41:04 +0300

    Update spotify.js

--- assets/js/spotify.js
+++ assets/js/spotify.js
@@ -1,73 +1,29 @@
-let lastTrackLink = '';
-let fetchIntervalId = null;
+    let lastTrackLink = '';
 
-function getSettings() {
-  const linkColor = localStorage.getItem("linkColor") || "#53a245";
-  return { linkColor };
-}
-
-function applySettings() {
-  const settings = getSettings();
-  
-  const trackLink = document.querySelector('#now-playing a');
-  if (trackLink) {
-    trackLink.style.color = settings.linkColor;
-  }
-}
-
-function formatTime(seconds) {
-  const minutes = Math.floor(seconds / 60);
-  seconds = seconds % 60;
-  return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
-}
-
-function fetchTrackData() {
-  fetch('https://spotify-now-playing-akakadirs-projects.vercel.app/api/now-playing')
-    .then(response => response.json())
-    .then(data => {
-      if (data.error) {
-        document.getElementById('now-playing').innerHTML = data.error;
-      } else {
-        if (data.trackLink !== lastTrackLink) {
-          lastTrackLink = data.trackLink;
-        }
-        const progressParts = data.progress.split(':').map(Number);
-        const durationParts = data.duration.split(':').map(Number);
-        const currentProgressSeconds = progressParts[0] * 60 + progressParts[1];
-        const totalDurationSeconds = durationParts[0] * 60 + durationParts[1];
-        const trackLinkHTML = `<a href="${data.trackLink}" target="_blank">${data.name}</a>`;
-        document.getElementById('now-playing').innerHTML = `🎧 Şu an dinliyorum: `+`${data.artists} - ${trackLinkHTML} | ${formatTime(currentProgressSeconds)}/${data.duration}`;
-        
-        applySettings();
-      }
-    });
-}
-
-function startFetching() {
-  if (!fetchIntervalId) {
-    fetchTrackData();
-    fetchIntervalId = setInterval(fetchTrackData, 1000);
-  }
-}
-
-function stopFetching() {
-  if (fetchIntervalId) {
-    clearInterval(fetchIntervalId);
-    fetchIntervalId = null;
-  }
-}
-
-(function observeModal() {
-  const modalObserver = new MutationObserver(() => {
-    const isModalOpen = window.isModalOpen;
-    if (isModalOpen) {
-      stopFetching();
-    } else {
-      startFetching();
+    function formatTime(seconds) {
+      const minutes = Math.floor(seconds / 60);
+      seconds = seconds % 60;
+      return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
     }
-  });
-
-  modalObserver.observe(document.body, { attributes: true, childList: true, subtree: true });
-
-  startFetching();
-})();
+    
+    function fetchTrackData() {
+      fetch('https://spotify-now-playing-akakadirs-projects.vercel.app/api/now-playing')
+        .then(response => response.json())
+        .then(data => {
+          if (data.error) {
+            document.getElementById('now-playing').innerHTML = data.error;
+          } else {
+            if (data.trackLink !== lastTrackLink) {
+              lastTrackLink = data.trackLink;
+            }
+            const progressParts = data.progress.split(':').map(Number);
+            const durationParts = data.duration.split(':').map(Number);
+            const currentProgressSeconds = progressParts[0] * 60 + progressParts[1];
+            const totalDurationSeconds = durationParts[0] * 60 + durationParts[1];
+            const trackLinkHTML = `<a class="default" href="${data.trackLink}" target="_blank">${data.name}</a>`;
+            document.getElementById('now-playing').innerHTML = `🎧 Şu an dinliyorum: `+`${data.artists} - ${trackLinkHTML} | ${formatTime(currentProgressSeconds)}/${data.duration}`;
+          }
+        });
+    }
+    fetchTrackData();
+    setInterval(fetchTrackData, 1000);
