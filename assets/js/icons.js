7a5e9d260f3075aa66abbe5fdc17323244732d89
Kadir <101037506+akakadir@users.noreply.github.com>
2026-04-17 07:55:33 +0300

    Update icons.js

--- assets/js/icons.js
+++ assets/js/icons.js
@@ -6,11 +6,10 @@ document.addEventListener("DOMContentLoaded", function() {
         if (i) {
             if (i.startsWith("mailto:")) s = "/assets/img/png/formail.png";
             else if (i.toLowerCase().endsWith(".pdf")) s = "/assets/img/png/forpdf.png";
-            else if (i.toLowerCase().endsWith(".png")) s = "/assets/img/png/forimg.png";
             else if (i.startsWith("http://") || i.startsWith("https://")) {
                 let n = new URL(i),
                     a = n.hostname;
-                "github.com" === a ? s = "/assets/img/png/github.png" : "twitter.com" === a || "x.com" === a ? s = "/assets/img/png/twitter.png" : "files.catbox.moe" === a ? s = "/assets/img/png/forimg.png" : a !== t && (s = `https://www.google.com/s2/favicons?domain=${a}&sz=256`)
+                "github.com" === a ? s = "/assets/img/png/github.png" : "twitter.com" === a || "x.com" === a ? s = "/assets/img/png/twitter.png" : "files.catbox.moe" === a ? s = "/assets/img/png/forimg.png?v=2" : a !== t && (s = `https://www.google.com/s2/favicons?domain=${a}&sz=256`)
             }
             if (s) {
                 let o = document.createElement("img");
